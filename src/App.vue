af809b8699939ec11b4728eb5db0dfff3164f7e5
ashish <ashishpok@gmail.com>
2024-12-18 23:03:30 -0500

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <!-- Navbar component for global navigation -->
+    <Navbar />
+    
+    <!-- Router View: This is where the routed pages will be injected -->
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Importing the Navbar component
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
-}
+};
 </script>
 
 <style>
+/* You can add global styles here */
+body {
+  font-family: Arial, sans-serif;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <div class="banner-container" style="background-image: url('@/assets/banner.jpg'); height: 300px; background-size: cover;">
+        <h1 class="text-center text-white pt-5">Welcome to Electrolysis Associates</h1>
+      </div>
+      <div class="container mt-4">
+        <p>Your trusted provider of permanent hair removal services in Ashburn and Vienna, VA.</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home',
+  };
+  </script>
+  
+  <style scoped>
+  .banner-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  </style>
+  
