cc38140312bba468326caf8e3f36ec1eca3d9886
ashish <ashishpok@gmail.com>
2024-12-19 08:38:45 -0500

    Initial commit

---
+++ src/components/About.vue
@@ -0,0 +1,22 @@
+<!-- src/views/About.vue -->
+<template>
+    <div>
+      <h1>About Electrolysis Associates</h1>
+      <p>We provide electrolysis hair removal services, the only permanent hair removal technology. Our services are available in Ashburn and Vienna, VA.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'About',
+  };
+  </script>
+  
+  <style scoped>
+  /* Add styles for the About page */
+  h1 {
+    font-size: 2rem;
+    color: #333;
+  }
+  </style>
+  
---
+++ src/components/Location.vue
@@ -0,0 +1,27 @@
+<!-- src/views/Location.vue -->
+<template>
+    <div>
+      <h1>Our Locations</h1>
+      <p>We offer our services at two convenient locations:</p>
+      <ul>
+        <li>Ashburn, VA</li>
+        <li>Vienna, VA</li>
+      </ul>
+      <p>Visit us at these locations for professional electrolysis services!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Location',
+  };
+  </script>
+  
+  <style scoped>
+  /* Add styles for the Location page */
+  h1 {
+    font-size: 2rem;
+    color: #333;
+  }
+  </style>
+  
