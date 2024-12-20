d173a32eb84cccb7d831d7944d74f94a63f02854
ashish <ashishpok@gmail.com>
2024-12-19 23:07:17 -0500

    Saving before destroying

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,12 +1,12 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Electrolysis Associates</a>
       <button
         class="navbar-toggler"
         type="button"
         data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
+        data-bs-target="# "
         aria-controls="navbarNav"
         aria-expanded="false"
         aria-label="Toggle navigation"
